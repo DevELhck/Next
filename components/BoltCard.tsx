863f3a058ffabf7994af569a96678a3894c7fe9f
Emmanuel <elwintg@gmail.com>
2025-09-08 01:57:52 -0700

    try

--- components/BoltCard.tsx
+++ components/BoltCard.tsx
@@ -1,23 +1,45 @@
-import React from "react";
-
-interface BoltCardProps {
-  title: string;
-  descriptions: string;
-  date: string;
-  image?: string;
-}
-
-const BoltCard: React.FC<BoltCardProps> = ({ title, descriptions, date, image }) => {
+const BoltCard = ({
+  title,
+  desc,
+  date,
+}: {
+  title: string
+  desc: string
+  date: string
+}) => {
   return (
-    <div className="flex-1 rounded bg-white shadow overflow-hidden">
-      {image && <img src={image} alt={title} className="w-full h-48 object-cover" />}
-      <div className="p-4 flex flex-col gap-3">
-        <span className="font-medium">{title}</span>
-        <h3 className="font-bold text-lg leading-5 tracking-tight">{descriptions}</h3>
+    <div className="flex-1 rounded bg-white shadow overflow-hidden group">
+      <img
+        className="h-52 w-full object-cover group-hover:scale-110 transition duration-500"
+        src="https://res.cloudinary.com/dugcmkito/image/upload/v1751315857/Ryan_5_4_bca957ef3e.png"
+        alt=""
+      />
+
+      <div className="p-4 flex flex-col gap-3 group/bottom">
+        <span className="font-medium flex items-center gap-2">
+          {title}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-move-right-icon lucide-move-right group-hover/bottom:translate-x-4 transition"
+          >
+            <path d="M18 8L22 12L18 16" />
+            <path d="M2 12H22" />
+          </svg>
+        </span>
+        <h3 className="font-bold text-lg leading-5 tracking-tight">{desc}</h3>
+
         <span className="text-sm">{date}</span>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BoltCard;
+export default BoltCard
